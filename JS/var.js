27df77acfdd2a48b808e12f4a1514cc9187c545e
Velda Kiara <veldakarimi@gmail.com>
2020-06-07 22:58:33 +0300

    added Js folder

---
+++ JS/var.js
@@ -0,0 +1,22 @@
+let number = 1;
+
+console.log(number);
+number += 1
+
+console.log(number);
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
+number += 1;
+
+//iteration using while loop
+let number = 1;
+
+// Add a while loop here
+while(number <= 100){
+  console.log(number);
+  number += 1;
+}
