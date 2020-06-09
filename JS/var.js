28fc7e456df7f2d4c66db8d4c389d50d8806c2d4
Velda Kiara <veldakarimi@gmail.com>
2020-06-10 01:11:08 +0300

    objects and their properties and arrays and referencing

--- JS/var.js
+++ JS/var.js
@@ -76,3 +76,16 @@ const characters = [
 ];
 console.log(characters[0]);
 console.log(characters[1].name);
+
+//objects and properties, arrays and referencing them
+const characters = [
+  {name: "Ken the Ninja", age: 14},
+  {name: "Master White", age: 100},
+  {name: "Ben the Baby Ninja", age: 5},
+];
+for (let i = 0; i < characters.length; i++) {
+  console.log("--------------------");
+  const character = characters[i];
+  console.log(`My name is  ${character.name}`);
+  console.log(`I am ${character.age} years old`)
+}
