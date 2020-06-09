4b9ac072177a24c521ed775c9238a0352d1ee505
Velda Kiara <veldakarimi@gmail.com>
2020-06-10 00:56:20 +0300

    combining arrays, objects and their properties

--- JS/var.js
+++ JS/var.js
@@ -63,4 +63,16 @@ for (let i = 0; i < animals.length; i++) {
 const character ={name:"Ken the Ninja", age:14};
 console.log(character)
 
+//updating value of objects properties
+const character = {name: "Ken the Ninja", age: 14};
+console.log(character.name);
+character[age]=20;
+console.log(character);
 
+//combinig arrays, objects and properties
+const characters = [
+  {name: "Ken the Ninja", age: 14},
+  {name: "Master White", age: 1000}
+];
+console.log(characters[0]);
+console.log(characters[1].name);
