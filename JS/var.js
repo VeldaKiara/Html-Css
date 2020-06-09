c178021aa20ded41c182cc52dc1d83a1266b19ae
Velda Kiara <veldakarimi@gmail.com>
2020-06-09 18:36:45 +0300

    iteration using for loop

--- JS/var.js
+++ JS/var.js
@@ -15,8 +15,14 @@ number += 1;
 //iteration using while loop
 let number = 1;
 
-// Add a while loop here
+// Add a while loop here, prints 1-100
 while(number <= 100){
   console.log(number);
   number += 1;
 }
+//using for loop like list comprehensions in py
+//number++ is equal to number += 1
+for(let number=1; number<= 100; number++ ){
+  console.log(number);
+}
+
