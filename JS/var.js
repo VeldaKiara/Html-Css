01b6219ef790b029ce7b1f817af871ec75400b52
Velda Kiara <veldakarimi@gmail.com>
2020-06-09 18:44:24 +0300

    multiples of 3

--- JS/var.js
+++ JS/var.js
@@ -20,9 +20,17 @@ while(number <= 100){
   console.log(number);
   number += 1;
 }
+
 //using for loop like list comprehensions in py
 //number++ is equal to number += 1
 for(let number=1; number<= 100; number++ ){
   console.log(number);
 }
-
+//for and if statements to output multiples of 3
+for (let number = 1; number <= 100 ; number ++) {
+  if(number % 3 == 0){
+    console.log("Multiple of 3");
+  }else{
+    console.log(number)
+  }
+}
