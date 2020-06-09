799a36578fa1b903f7b4813226eeeb035db61c78
Velda Kiara <veldakarimi@gmail.com>
2020-06-10 00:41:34 +0300

    adding objects and properties

--- JS/var.js
+++ JS/var.js
@@ -59,5 +59,8 @@ console.log(animals.length)
 for (let i = 0; i < animals.length; i++) {
   console.log(animals[i]);
 }
+//using objects and properties
+const character ={name:"Ken the Ninja", age:14};
+console.log(character)
 
 
