375879d486e223d10f8caa1f4b44faca72bdff53
Velda Kiara <veldakarimi@gmail.com>
2020-06-10 04:19:10 +0300

    getting max using func

--- JS/func.js
+++ JS/func.js
@@ -62,3 +62,22 @@ const introduce = (name) => {
 introduce("Master White");
 
 console.log(name)
+
+const number1 = 103;
+const number2 = 72;
+const number3 = 189;
+
+// Write a function called getMax to get the maximum value
+const getMax = (a, b, c) =>{
+  let max = a;
+  if (b > max){
+    max = b;
+  }
+  if(c > max){
+    max = c;
+  }
+  return max;
+};
+
+const max = getMax(number1, number2, number3);
+console.log(`The maximum value is ${max}`);
