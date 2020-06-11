122a7096a0ad6efcc3e660e2db250596d7cb2b51
Velda Kiara <veldakarimi@gmail.com>
2020-06-11 15:05:42 +0300

    inheritance

--- JS/class.js
+++ JS/class.js
@@ -43,13 +43,18 @@ const animal ={
             console.log("Hello");
           }
           info(){
+            this.greet();
             console.log(`My name is ${this.name}`);
             console.log(`I'm ${this.age} years old`);
           }
       }
-      
+    class Dog extends Animal {
+  
+    }
+
       const animal = new Animal("Mocha", 8);
-      
+      const dog = new Dog("Leo", 4);
+      dog.info();
       console.log(`Name: ${animal.name}`);
       console.log(`Age: ${animal.age}`);
-      animal.greet();
+      animal.info();
