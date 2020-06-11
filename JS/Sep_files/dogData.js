f2fdc5f60d2f9cc24f2b7e171e1c070daba7f465
Velda Kiara <veldakarimi@gmail.com>
2020-06-11 17:37:31 +0300

    exporting values

--- JS/Sep_files/dogData.js
+++ JS/Sep_files/dogData.js
@@ -1,5 +1,6 @@
 import Dog from "./Sep_files/dog.js";
 
-const dog = new Dog("Leo", 4, "Chihuahua");
+const dog1 = new Dog("Leo", 4, "Chihuahua");
+const dog2 = new Dog("Ben", 2, "Poodle");
 
-export default dog;
+export { dog1, dog2};
--- JS/Sep_files/script.js
+++ JS/Sep_files/script.js
@@ -1,2 +1,6 @@
-import dog from "./Sep_files/dogData.js";
-dog.info();
+import {dog1, dog2} from "./Sep_files/dogData.js";
+
+console.log("---------");
+dog1.info();
+console.log("---------");
+dog2.info();
