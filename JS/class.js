83eafb7c729b8bb33cc511404d922fd3b14edf48
Velda Kiara <veldakarimi@gmail.com>
2020-06-11 14:06:03 +0300

    adding classes and constructors

---
+++ JS/class.js
@@ -0,0 +1,22 @@
+const animal ={
+    name:"Leo",
+    age: 3,
+    greet: () => {
+      console.log("Hello");
+    }
+    
+  };
+  
+  console.log(animal.name);
+  animal.greet()
+
+  class Animal {
+    constructor (){
+      console.log("Created a new instance");
+    }
+      
+    
+    }
+    
+    const animal = new Animal();
+    
