5989fd19aab284103e827f1552fa582c9e3626df
Velda Kiara <veldakarimi@gmail.com>
2020-06-12 20:04:33 +0300

    adding push method to add items in an arraay

---
+++ JS/arrays.js
@@ -0,0 +1,5 @@
+//using push method to add items to the arrays.
+const characters = ["Velda the Developer", "Karimi the painter", "VeldaKiara the best"];
+console.log(characters);
+characters.push("Blogger");
+console.log(characters);
