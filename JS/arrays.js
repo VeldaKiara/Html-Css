3c5d7f72d764159722b42cddbaf9e56ea433ef79
Velda Kiara <veldakarimi@gmail.com>
2020-06-12 21:18:46 +0300

    addition of the map method

--- JS/arrays.js
+++ JS/arrays.js
@@ -50,3 +50,25 @@ const underTwenty = characters.filter((character) => {
 }
 );
 console.log(underTwenty);
+
+// map method which applies the same code to evevry value in an array
+const numbers = [1, 2, 3, 4];
+const doubledNumbers = numbers.map((number) =>{
+  return number*2;
+}
+);
+console.log(doubledNumbers);
+
+
+const names = [
+  {firstName: "Kate", lastName: "Jones"},
+	{firstName: "John", lastName: "Smith"},
+	{firstName: "Dennis", lastName: "Williams"},
+	{firstName: "David", lastName: "Black"}
+];
+
+const fullNames = names.map((name) =>{
+  return name.firstName + " " + name.lastName;
+}
+);
+console.log(fullNames)
