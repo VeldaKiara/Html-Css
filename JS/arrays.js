7bd1865e6db8b42383b3118d0b5a51ef3a0c6d4e
Velda Kiara <veldakarimi@gmail.com>
2020-06-12 20:29:34 +0300

    using the find method, as an arrow func

--- JS/arrays.js
+++ JS/arrays.js
@@ -11,3 +11,23 @@ characters.forEach((character) => {
   console.log(character)
 }
 );
+
+//using find method
+const numbers = [1, 3, 5, 7, 9];
+const foundNumber = numbers.find((number) => {
+   return number % 3 === 0;
+ }
+ );
+console.log(foundNumber);
+const characters = [
+  {id: 1, name: "Velda the Developer", age: 16},
+  {id: 2, name: "Karimi the painter", age: 21},
+  {id: 3, name: "Velda Kiara the best", age: 18},
+  {id: 4, name: "Velda", age: 22}
+];
+
+const foundCharacter = characters.find((character) => {
+  return character.id === 3;
+}
+);
+console.log(foundCharacter);
